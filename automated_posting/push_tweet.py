1b5b00a37a8f26fef418e13234bc93b4d8b5bf96
rajaswa <rajp4480@gmail.com>
2018-10-21 16:22:53 +0530

    need to add API key for twitter

--- automated_posting/push_tweet.py
+++ automated_posting/push_tweet.py
@@ -1,11 +1,11 @@
 import tweepy
 
 #tokens and keys
-consumer_key = "FR3xuN0UYgdMpngpaj8gtnAvU"
-consumer_secret = "JeGf8tWdPROcWAbrEELtvSk86wYzGi9mv0kOsIQdJUQevK1oVD"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "2942900654-SXdsN5ziOiW8pogcSP0DXGWsL5RIkHV5uLwbzGa"
-access_token_secret = "04BIhJAYxUVyOxgzjlXpxd5p6meihPZM48I3iMDifD7Hd"
+access_token = ""
+access_token_secret = ""
 
 #authorization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
