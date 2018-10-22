eaa858f48205c453d1dc17804d0d74db56f45397
rajaswa <rajp4480@gmail.com>
2018-10-22 20:56:21 +0530

    check previous commit

--- scripts/push_tweet.py
+++ scripts/push_tweet.py
@@ -1,11 +1,11 @@
 import tweepy
 
 #tokens and keys
-consumer_key = "nHrP4zQdr9W6qDuw6dtmzmcas"
-consumer_secret = "kelpyn6orbvgP6RvUdhNnWpHWooUdBKb49GiI262Tr2RQ8Kisj"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "2942900654-SXdsN5ziOiW8pogcSP0DXGWsL5RIkHV5uLwbzGa"
-access_token_secret = "04BIhJAYxUVyOxgzjlXpxd5p6meihPZM48I3iMDifD7Hd"
+access_token = ""
+access_token_secret = ""
 
 #authorization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
